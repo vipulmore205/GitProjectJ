5764db74488e7a32da6024c76a2ca8a0a84edd0c
vipulmore205 <vipulmore205@gmail.com>
2025-07-21 11:24:09 +0530

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll("a[href^='#']").forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
